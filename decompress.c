33a3c4a10db27fba05baefab838644fb1120fb91
Orion Chandler <jelly69105@gmail.com>
2024-03-17 09:58:01 -0500

    Fixed my stupidity

--- decompress.c
+++ decompress.c
@@ -99,7 +99,11 @@ void decompress(uint8_t *indata, int insize, struct compnode *nodes)
                                 nodes->size = *indata & 0x07;
                                 nodes->offset = (*indata++ & 0xF8) >> 3;
                                 nodes->offset |= *indata++ << 5;
+                                //Special detect for the end phrase
                                 if (!nodes->size && nodes->offset) {
+                                        nodes->size--;
+                                }
+                                if (!nodes->size) {
                                         nodes->size = *indata++;
                                 } else {
                                         nodes->size++;
